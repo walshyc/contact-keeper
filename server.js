eae59a4d6643a1fc2fd79710cacb33dd341a7952
Conor Walsh <conorwalsh0703@gmail.com>
2020-02-12 19:37:42 +0000

    setup routes

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const app = express()
 
 app.get('/', (req, res)=> res.json({msg: 'Welcome to the ContactKepper API...'}))
 
+// Define Routes
+
+app.use('/api/users', require('./routes/users'))
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/contacts', require('./routes/contacts'))
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`))
