7ba7965d0483376d9324040e1add2cee3f13584a
Conor Walsh <conorwalsh0703@gmail.com>
2020-02-17 16:03:30 +0000

    add loadUser to home

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,18 +1,28 @@
-import React from 'react'
-import Contacts from '../contacts/Contacts'
-import ContactForm from '../contacts/ContactForm'
-import ContactFilter from '../contacts/ContactFilter'
+import React, { useContext, useEffect } from "react";
+import Contacts from "../contacts/Contacts";
+import ContactForm from "../contacts/ContactForm";
+import ContactFilter from "../contacts/ContactFilter";
+import AuthContext from "../../context/auth/authContext";
 
 const Home = () => {
-    return (
-        <div className="grid-2">
-            <div className="">
-                <ContactForm></ContactForm>
-            </div>
-            <div className=""><ContactFilter></ContactFilter><Contacts></Contacts></div>
-            
-        </div>
-    )
-}
+  const authContext = useContext(AuthContext);
+  const { loadUser } = authContext;
 
-export default Home
+  useEffect(()=> {
+      loadUser()
+      // eslint-disable-next-line
+  },[])
+  return (
+    <div className="grid-2">
+      <div className="">
+        <ContactForm></ContactForm>
+      </div>
+      <div className="">
+        <ContactFilter></ContactFilter>
+        <Contacts></Contacts>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -2,7 +2,8 @@ import React, { useReducer } from "react";
 import axios from "axios";
 import AuthContext from "./authContext";
 import authReducer from "./authReducer";
-import {
+import setAuthToken from '../../utils/setAuthToken'
+import {  
   LOGIN_FAIL,
   LOGOUT,
   LOGIN_SUCCESS,
@@ -25,7 +26,26 @@ const AuthState = props => {
   const [state, dispatch] = useReducer(authReducer, initialState);
 
   // Load User
-  const loadUser = () => {};
+  const loadUser = async () => {
+    if (localStorage.token) {
+      setAuthToken(localStorage.token)
+    }
+
+    try {
+      const res = await axios.get("/api/auth");
+
+      dispatch({
+        type: USER_LOADED,
+        payload: res.data
+      });
+
+      
+    } catch (error) {
+      dispatch({
+        type: AUTH_ERROR
+      })
+    }
+  };
 
   // Register User
   const register = async formData => {
@@ -42,6 +62,7 @@ const AuthState = props => {
         type: REGISTER_SUCCESS,
         payload: res.data
       });
+      loadUser()
     } catch (error) {
       dispatch({
         type: REGISTER_FAIL,
@@ -58,7 +79,7 @@ const AuthState = props => {
 
   // Clear Errors
   const clearErrors = () => {
-    dispatch({type: CLEAR_ERRORS})
+    dispatch({ type: CLEAR_ERRORS });
   };
 
   return (
