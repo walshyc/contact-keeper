a4c69b6677ed59a1a3d0b9f1aa21c99a8636f1bc
Conor Walsh <conorwalsh0703@gmail.com>
2020-02-17 19:38:47 +0000

    added deployment setup

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
 const express = require("express");
-const connectDB = require('./config/db')
+const connectDB = require("./config/db");
+const path = require('path')
 
-const app = express()
+const app = express();
 
 // Connect Database
-connectDB()
+connectDB();
 
 // Init Middleware
-app.use(express.json({extended: false}))
-
-
-app.get('/', (req, res)=> res.json({msg: 'Welcome to the ContactKepper API...'}))
+app.use(express.json({ extended: false }));
 
 // Define Routes
 
-app.use('/api/users', require('./routes/users'))
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/contacts', require('./routes/contacts'))
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
 
-const PORT = process.env.PORT || 5000
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
 
-app.listen(PORT, () => console.log(`Server started on ${PORT}`))
+  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
